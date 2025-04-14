0c699e3c76e88e1a1aa8165fd3ee1674b781ee60
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-04-14 16:29:09 +0000

    FIX: Simplified `shell.nix`

--- shell.nix
+++ shell.nix
@@ -2,24 +2,10 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.zsh
-    pkgs.git
-    pkgs.subversion
     pkgs.gcc
   ];
 
   shellHook = ''
-    alias vim="nvim"
-    alias vi="nvim"
-    alias v="nvim"
-
-    # Modify this
-    export PS1="\[\e[35m\]\w \$ \[\e[0m\]"
-
-    echo ""
-    echo "The default environment is ready!"
-    echo ""
-
     zsh
   '';
 }
